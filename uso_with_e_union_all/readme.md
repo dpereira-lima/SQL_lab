9adb6adcbc1727ec1566eea9bfe3cba587a4daef
Diego Pereira de Lima <66127839+dpereira-lima@users.noreply.github.com>
2026-07-21 14:49:30 -0300

    Add files via upload

---
+++ uso_with_e_union_all/readme.md
@@ -0,0 +1,17 @@
+## Explicação em Português
+
+Os scripts deste diretório contêm consultas em SQL Oracle.
+
+O script **old** realiza a consulta dos dados da base utilizando funções de linha simples e funções de grupo, aninhadas.
+
+O script **new** utiliza a mesma lógica do script **old**, porém incorpora as cláusulas **WITH** e **UNION ALL** para apresentar os mesmos dados em um formato equivalente ao **UNPIVOT**, facilitando a transformação das colunas em linhas e tornando o resultado mais adequado para análises e integrações.
+
+
+
+## Explanation in English
+
+The scripts in this directory contain Oracle SQL queries.
+
+The **old** script queries the database data using single row functions and nested group functions.
+
+The **new** script uses the same logic as the **old** script, but incorporates the **WITH** and **UNION ALL** clauses to present the same data in a format equivalent to **UNPIVOT**, facilitating the transformation of columns into rows and making the result more suitable for analysis and integration.
---
+++ uso_with_e_union_all/uso_with_e_union_all_old.sql
@@ -0,0 +1,28 @@
+select
+    decode(cua.nr_sequencia, 9, 'Onco Clínica', cua.ds_classif) ds_clinica,
+	trunc(avg(nullif(to_number(obter_diferen_dt(a.dt_historico, a.dt_fim_historico, 'TM')), 0))) qt_avg_min_proces_alta,
+	trunc(avg(nullif(to_number(obter_diferen_dt(obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'I', 'I','N'), obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'F', 'I','N'), 'TM')), 0))) qt_avg_min_interd,
+	trunc(avg(nullif(to_number(obter_diferen_dt(obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'I', 'S','N'), obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'F', 'S','N'), 'TM')), 0))) qt_avg_min_saida_interd,
+	trunc(avg(nullif(to_number(obter_diferen_dt(obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'I', 'G','N'), obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'F', 'G','N'), 'TM')), 0))) qt_avg_min_ag_higi,
+	trunc(avg(nullif(to_number(obter_diferen_dt(obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'I', 'H','N'), obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'F', 'H','N'), 'TM')), 0))) qt_avg_min_em_higi,
+	trunc(avg(nullif(to_number(obter_diferen_dt(obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'I', 'R','N'), obter_datas_status_leito(a.nr_seq_unidade, a.nr_sequencia, 'F', 'R','N'), 'TM')), 0))) qt_avg_min_res
+from unidade_atend b
+    inner join setor_atend sa
+        on b.cd_setor_atend = sa.cd_setor_atend
+        and upper(sa.ds_setor_atend) not like '%PS%'
+        and upper(sa.ds_setor_atend) not like '%RADIOL%'
+        and upper(sa.ds_setor_atend) not like '%HOSP%'
+        and upper(sa.ds_setor_atend) not like '%CIRUR%'
+    inner join un_atend_hist a
+        on b.nr_seq_interno = a.nr_seq_unidade
+        and a.ie_status_unidade = 'A'
+        and trunc(a.dt_historico) between trunc(sysdate - 90) and trunc(sysdate -1)
+    left join base_status vl
+        on b.ie_status_unidade = vl.ie_base_status
+    left join base_classif_setor vl_2
+        on sa.cd_classif_setor = vl_2.cd_base_classif_setor
+    left join classif_un_atend cua
+        on b.nr_seq_classif = cua.nr_sequencia
+where b.ie_situacao = 'A'
+group by decode(cua.nr_sequencia, 9, 'Onco Clínica', cua.ds_classificacao)
+;
